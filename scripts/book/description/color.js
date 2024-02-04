3bc2a5f9a4bef1f035218c12699d7ef410311a1c
Inoshiro90 <110790047+Inoshiro90@users.noreply.github.com>
2024-02-04 23:37:02 +0100

    commit

--- scripts/book/description/color.js
+++ scripts/book/description/color.js
@@ -1,698 +1,698 @@
 const color = [
 	{
 		name: 'achatgrau',
-		hex: '#ADB0A9',
+		hex: '#ADB0A9'
 	},
 	{
 		name: 'altrosa',
-		hex: '#BC6F72',
+		hex: '#BC6F72'
 	},
 	{
 		name: 'anthrazitgrau',
-		hex: '#BC6F72',
+		hex: '#383E42'
 	},
 	{
 		name: 'azurblau',
-		hex: '#365875',
+		hex: '#365875'
 	},
 	{
 		name: 'basaltgrau',
-		hex: '#595E60',
+		hex: '#595E60'
 	},
 	{
 		name: 'beige',
-		hex: '#C6B286',
+		hex: '#C6B286'
 	},
 	{
 		name: 'beigebraun',
-		hex: '#6F543C',
+		hex: '#6F543C'
 	},
 	{
 		name: 'beigegrau',
-		hex: '#7B858D',
+		hex: '#7B858D'
 	},
 	{
 		name: 'beigerot',
-		hex: '#B7856E',
+		hex: '#B7856E'
 	},
 	{
 		name: 'blassbraun',
-		hex: '#6E5B4B',
+		hex: '#6E5B4B'
 	},
 	{
 		name: 'blassgrün',
-		hex: '#899B79',
+		hex: '#899B79'
 	},
 	{
 		name: 'blaugrau',
-		hex: '#5D676D',
+		hex: '#5D676D'
 	},
 	{
 		name: 'blaugrün',
-		hex: '#214245',
+		hex: '#214245'
 	},
 	{
 		name: 'blaulila',
-		hex: '#746395',
+		hex: '#746395'
 	},
 	{
 		name: 'blutorange',
-		hex: '#AC3721',
+		hex: '#AC3721'
 	},
 	{
 		name: 'bordeauxviolett',
-		hex: '#5F1837',
+		hex: '#5F1837'
 	},
 	{
 		name: 'braunbeige',
-		hex: '#A38454',
+		hex: '#A38454'
 	},
 	{
 		name: 'braungrau',
-		hex: '#545146',
+		hex: '#545146'
 	},
 	{
 		name: 'braungrün',
-		hex: '#333327',
+		hex: '#333327'
 	},
 	{
 		name: 'braunoliv',
-		hex: '#3B382E',
+		hex: '#3B382E'
 	},
 	{
 		name: 'braunrot',
-		hex: '#6E2124',
+		hex: '#6E2124'
 	},
 	{
 		name: 'brillantblau',
-		hex: '#466589',
+		hex: '#466589'
 	},
 	{
 		name: 'capriblau',
-		hex: '#265682',
+		hex: '#265682'
 	},
 	{
 		name: 'cremeweiß',
-		hex: '#E5E1D4',
+		hex: '#E5E1D4'
 	},
 	{
 		name: 'currygelb',
-		hex: '#998420',
+		hex: '#998420'
 	},
 	{
 		name: 'dahliengelb',
-		hex: '#E4A02D',
+		hex: '#E4A02D'
 	},
 	{
 		name: 'eisengrau',
-		hex: '#535A5E',
+		hex: '#535A5E'
 	},
 	{
 		name: 'elfenbein',
-		hex: '#D4C79C',
+		hex: '#D4C79C'
 	},
 	{
 		name: 'enzianblau',
-		hex: '#0E457A',
+		hex: '#0E457A'
 	},
 	{
 		name: 'erdbeerrot',
-		hex: '#B63C49',
+		hex: '#B63C49'
 	},
 	{
 		name: 'farngrün',
-		hex: '#5D703E',
+		hex: '#5D703E'
 	},
 	{
 		name: 'fernblau',
-		hex: '#4D648A',
+		hex: '#4D648A'
 	},
 	{
 		name: 'feuerrot',
-		hex: '#962A27',
+		hex: '#962A27'
 	},
 	{
 		name: 'gelbgrau',
-		hex: '#8C8870',
+		hex: '#8C8870'
 	},
 	{
 		name: 'gelbgrün',
-		hex: '#689A45',
+		hex: '#689A45'
 	},
 	{
 		name: 'gelboliv',
-		hex: '#454339',
+		hex: '#454339'
 	},
 	{
 		name: 'gelborange',
-		hex: '#C7750F',
+		hex: '#C7750F'
 	},
 	{
 		name: 'ginstergelb',
-		hex: '#CFA81E',
+		hex: '#CFA81E'
 	},
 	{
 		name: 'goldgelb',
-		hex: '#CDA434',
+		hex: '#CDA434'
 	},
 	{
 		name: 'granitgrau',
-		hex: '#D2A40E',
+		hex: '#D2A40E'
 	},
 	{
 		name: 'graphitgrau',
-		hex: '#45494E',
+		hex: '#45494E'
 	},
 	{
 		name: 'graphitschwarz',
-		hex: '#25282A',
+		hex: '#25282A'
 	},
 	{
 		name: 'grasgrün',
-		hex: '#4E6E39',
+		hex: '#4E6E39'
 	},
 	{
 		name: 'graubeige',
-		hex: '#9C917B',
+		hex: '#9C917B'
 	},
 	{
 		name: 'graublau',
-		hex: '#2F3A44',
+		hex: '#2F3A44'
 	},
 	{
 		name: 'graubraun',
-		hex: '#3B3736',
+		hex: '#3B3736'
 	},
 	{
 		name: 'grauoliv',
-		hex: '#3C3D32',
+		hex: '#3C3D32'
 	},
 	{
 		name: 'grauweiß',
-		hex: '#D4D5CD',
+		hex: '#D4D5CD'
 	},
 	{
 		name: 'grünbeige',
-		hex: '#C5BB8A',
+		hex: '#C5BB8A'
 	},
 	{
 		name: 'grünblau',
-		hex: '#294763',
+		hex: '#294763'
 	},
 	{
 		name: 'grünbraun',
-		hex: '#816D44',
+		hex: '#816D44'
 	},
 	{
 		name: 'grüngrau',
-		hex: '#5B6058',
+		hex: '#5B6058'
 	},
 	{
 		name: 'hellelfenbein',
-		hex: '#DED3B6',
+		hex: '#DED3B6'
 	},
 	{
 		name: 'hellrosa',
-		hex: '#CC9EA4',
+		hex: '#CC9EA4'
 	},
 	{
 		name: 'hellrotorange',
-		hex: '#D66C21',
+		hex: '#D66C21'
 	},
 	{
 		name: 'himbeerrot',
-		hex: '#9E1B3C',
+		hex: '#9E1B3C'
 	},
 	{
 		name: 'himmelblau',
-		hex: '#3172AD',
+		hex: '#3172AD'
 	},
 	{
 		name: 'honiggelb',
-		hex: '#BC9611',
+		hex: '#BC9611'
 	},
 	{
 		name: 'karminrot',
-		hex: '#8D1F24',
+		hex: '#8D1F24'
 	},
 	{
 		name: 'kastanienbraun',
-		hex: '#57332B',
+		hex: '#57332B'
 	},
 	{
 		name: 'khakigrau',
-		hex: '#6C6040',
+		hex: '#6C6040'
 	},
 	{
 		name: 'kieferngrün',
-		hex: '#3D5547',
+		hex: '#3D5547'
 	},
 	{
 		name: 'kieselgrau',
-		hex: '#B1B1A1',
+		hex: '#B1B1A1'
 	},
 	{
 		name: 'kobaltblau',
-		hex: '#212F51',
+		hex: '#212F51'
 	},
 	{
 		name: 'korallenrot',
-		hex: '#963D2F',
+		hex: '#963D2F'
 	},
 	{
 		name: 'kupferbraun',
-		hex: '#814D37',
+		hex: '#814D37'
 	},
 	{
 		name: 'lachsorange',
-		hex: '#C2674F',
+		hex: '#C2674F'
 	},
 	{
 		name: 'lachsrot',
-		hex: '#BE6954',
+		hex: '#BE6954'
 	},
 	{
 		name: 'laubgrün',
-		hex: '#3B5B2F',
+		hex: '#3B5B2F'
 	},
 	{
 		name: 'lehmbraun',
-		hex: '#74502F',
+		hex: '#74502F'
 	},
 	{
 		name: 'leuchtgelb',
-		hex: '#FFFF00',
+		hex: '#FFFF00'
 	},
 	{
 		name: 'leuchtgrün',
-		hex: '#20A339',
+		hex: '#20A339'
 	},
 	{
 		name: 'leuchthellorange',
-		hex: '#FFAD19',
+		hex: '#FFAD19'
 	},
 	{
 		name: 'leuchtorange',
-		hex: '#FF4612',
+		hex: '#FF4612'
 	},
 	{
 		name: 'lichtblau',
-		hex: '#457FB3',
+		hex: '#457FB3'
 	},
 	{
 		name: 'lichtgrau',
-		hex: '#C2C6C3',
+		hex: '#C2C6C3'
 	},
 	{
 		name: 'lichtgrün',
-		hex: '#88B5B3',
+		hex: '#88B5B3'
 	},
 	{
 		name: 'mahagonibraun',
-		hex: '#483026',
+		hex: '#483026'
 	},
 	{
 		name: 'maigrün',
-		hex: '#5C8144',
+		hex: '#5C8144'
 	},
 	{
 		name: 'maisgelb',
-		hex: '#CF9804',
+		hex: '#CF9804'
 	},
 	{
 		name: 'mausgrau',
-		hex: '#696D6B',
+		hex: '#696D6B'
 	},
 	{
 		name: 'melonengelb',
-		hex: '#F2A500',
+		hex: '#F2A500'
 	},
 	{
 		name: 'minttürkis',
-		hex: '#568480',
+		hex: '#568480'
 	},
 	{
 		name: 'minzgrün',
-		hex: '#226C45',
+		hex: '#226C45'
 	},
 	{
 		name: 'moosgrau',
-		hex: '#76776A',
+		hex: '#76776A'
 	},
 	{
 		name: 'moosgrün',
-		hex: '#234235',
+		hex: '#234235'
 	},
 	{
 		name: 'nachtblau',
-		hex: '#282C58',
+		hex: '#282C58'
 	},
 	{
 		name: 'narzissengelb',
-		hex: '#D49300',
+		hex: '#D49300'
 	},
 	{
 		name: 'nussbraun',
-		hex: '#533A29',
+		hex: '#533A29'
 	},
 	{
 		name: 'ockerbraun',
-		hex: '#8F6833',
+		hex: '#8F6833'
 	},
 	{
 		name: 'ockergelb',
-		hex: '#AD9451',
+		hex: '#AD9451'
 	},
 	{
 		name: 'olivbraun',
-		hex: '#694F2B',
+		hex: '#694F2B'
 	},
 	{
 		name: 'olivgelb',
-		hex: '#999167',
+		hex: '#999167'
 	},
 	{
 		name: 'olivgrau',
-		hex: '#7D7965',
+		hex: '#7D7965'
 	},
 	{
 		name: 'olivgrün',
-		hex: '#4F553E',
+		hex: '#4F553E'
 	},
 	{
 		name: 'opalgrün',
-		hex: '#0D5951',
+		hex: '#0D5951'
 	},
 	{
 		name: 'orangebraun',
-		hex: '#965D33',
+		hex: '#965D33'
 	},
 	{
 		name: 'ozeanblau',
-		hex: '#113E4D',
+		hex: '#113E4D'
 	},
 	{
 		name: 'papyrusweiß',
-		hex: '#C6CBC6',
+		hex: '#C6CBC6'
 	},
 	{
 		name: 'pastellblau',
-		hex: '#6C8DAA',
+		hex: '#6C8DAA'
 	},
 	{
 		name: 'pastellgelb',
-		hex: '#D9A156',
+		hex: '#D9A156'
 	},
 	{
 		name: 'pastellorange',
-		hex: '#E17C30',
+		hex: '#E17C30'
 	},
 	{
 		name: 'pastelltürkis',
-		hex: '#86A9AD',
+		hex: '#86A9AD'
 	},
 	{
 		name: 'pastellviolett',
-		hex: '#95838F',
+		hex: '#95838F'
 	},
 	{
 		name: 'patinagrün',
-		hex: '#4A7363',
+		hex: '#4A7363'
 	},
 	{
 		name: 'perlbeige',
-		hex: '#898271',
+		hex: '#898271'
 	},
 	{
 		name: 'perlbrombeer',
-		hex: '#67657A',
+		hex: '#67657A'
 	},
 	{
 		name: 'perldunkelgrau',
-		hex: '#767779',
+		hex: '#767779'
 	},
 	{
 		name: 'perlgold',
-		hex: '#746341',
+		hex: '#746341'
 	},
 	{
 		name: 'perlgrün',
-		hex: '#2E4F31',
+		hex: '#2E4F31'
 	},
 	{
 		name: 'perlhellgrau',
-		hex: '#818382',
+		hex: '#818382'
 	},
 	{
 		name: 'perlkupfer',
-		hex: '#764537',
+		hex: '#764537'
 	},
 	{
 		name: 'perlmausgrau',
-		hex: '#7A7871',
+		hex: '#7A7871'
 	},
 	{
 		name: 'perlnachtblau',
-		hex: '#1B2B4D',
+		hex: '#1B2B4D'
 	},
 	{
 		name: 'perlopalgrün',
-		hex: '#27514A',
+		hex: '#27514A'
 	},
 	{
 		name: 'perlorange',
-		hex: '#824128',
+		hex: '#824128'
 	},
 	{
 		name: 'perlrosa',
-		hex: '#94352D',
+		hex: '#94352D'
 	},
 	{
 		name: 'perlrubinrot',
-		hex: '#661925',
+		hex: '#661925'
 	},
 	{
 		name: 'perlviolett',
-		hex: '#685C80',
+		hex: '#685C80'
 	},
 	{
 		name: 'perlweiß',
-		hex: '#DFDBC7',
+		hex: '#DFDBC7'
 	},
 	{
 		name: 'platingrau',
-		hex: '#949292',
+		hex: '#949292'
 	},
 	{
 		name: 'purpurrot',
-		hex: '#651927',
+		hex: '#651927'
 	},
 	{
 		name: 'purpurviolett',
-		hex: '#44263C',
+		hex: '#44263C'
 	},
 	{
 		name: 'quarzgrau',
-		hex: '#68675F',
+		hex: '#68675F'
 	},
 	{
 		name: 'rapsgelb',
-		hex: '#E5C000',
+		hex: '#E5C000'
 	},
 	{
 		name: 'rehbraun',
-		hex: '#67492F',
+		hex: '#67492F'
 	},
 	{
 		name: 'reingrün',
-		hex: '#3F8C3D',
+		hex: '#3F8C3D'
 	},
 	{
 		name: 'reinorange',
-		hex: '#CC5608',
+		hex: '#CC5608'
 	},
 	{
 		name: 'reinrot',
-		hex: '#B92726',
+		hex: '#B92726'
 	},
 	{
 		name: 'reinweiß',
-		hex: '#EFEEE5',
+		hex: '#EFEEE5'
 	},
 	{
 		name: 'resedagrün',
-		hex: '#6A7C5B',
+		hex: '#6A7C5B'
 	},
 	{
 		name: 'rosé',
-		hex: '#B9535B',
+		hex: '#B9535B'
 	},
 	{
 		name: 'rotbraun',
-		hex: '#5C3128',
+		hex: '#5C3128'
 	},
 	{
 		name: 'rotlila',
-		hex: '#7C5B80',
+		hex: '#7C5B80'
 	},
 	{
 		name: 'rotorange',
-		hex: '#A74D23',
+		hex: '#A74D23'
 	},
 	{
 		name: 'rotviolett',
-		hex: '#823A4B',
+		hex: '#823A4B'
 	},
 	{
 		name: 'rubinrot',
-		hex: '#7C0D24',
+		hex: '#7C0D24'
 	},
 	{
 		name: 'safrangelb',
-		hex: '#E4AF56',
+		hex: '#E4AF56'
 	},
 	{
 		name: 'sandgelb',
-		hex: '#C7AE72',
+		hex: '#C7AE72'
 	},
 	{
 		name: 'saphirblau',
-		hex: '#203151',
+		hex: '#203151'
 	},
 	{
 		name: 'schiefergrau',
-		hex: '#51535A',
+		hex: '#51535A'
 	},
 	{
 		name: 'schilfgrün',
-		hex: '#777659',
+		hex: '#777659'
 	},
 	{
 		name: 'schokoladenbraun',
-		hex: '#42332E',
+		hex: '#42332E'
 	},
 	{
 		name: 'schwarzblau',
-		hex: '#1E222C',
+		hex: '#1E222C'
 	},
 	{
 		name: 'schwarzbraun',
-		hex: '#201F20',
+		hex: '#201F20'
 	},
 	{
 		name: 'schwarzgrau',
-		hex: '#323537',
+		hex: '#323537'
 	},
 	{
 		name: 'schwarzgrün',
-		hex: '#2F3B39',
+		hex: '#2F3B39'
 	},
 	{
 		name: 'schwarzoliv',
-		hex: '#3C3F38',
+		hex: '#3C3F38'
 	},
 	{
 		name: 'schwarzrot',
-		hex: '#3D2326',
+		hex: '#3D2326'
 	},
 	{
 		name: 'schwefelgelb',
-		hex: '#E8E253',
+		hex: '#E8E253'
 	},
 	{
 		name: 'seidengrau',
-		hex: '#B3B2A9',
+		hex: '#B3B2A9'
 	},
 	{
 		name: 'sepiabraun',
-		hex: '#453729',
+		hex: '#453729'
 	},
 	{
 		name: 'silbergrau',
-		hex: '#8B949B',
+		hex: '#8B949B'
 	},
 	{
 		name: 'smaragdgrün',
-		hex: '#40693A',
+		hex: '#40693A'
 	},
 	{
 		name: 'sonnengelb',
-		hex: '#DB9A17',
+		hex: '#DB9A17'
 	},
 	{
 		name: 'stahlblau',
-		hex: '#222C3E',
+		hex: '#222C3E'
 	},
 	{
 		name: 'staubgrau',
-		hex: '#797B7B',
+		hex: '#797B7B'
 	},
 	{
 		name: 'steingrau',
-		hex: '#8C8C83',
+		hex: '#8C8C83'
 	},
 	{
 		name: 'tannengrün',
-		hex: '#2A372C',
+		hex: '#2A372C'
 	},
 	{
 		name: 'taubenblau',
-		hex: '#667691',
+		hex: '#667691'
 	},
 	{
 		name: 'terrabraun',
-		hex: '#4C3E30',
+		hex: '#4C3E30'
 	},
 	{
 		name: 'tieforange',
-		hex: '#CF7421',
+		hex: '#CF7421'
 	},
 	{
 		name: 'tiefschwarz',
-		hex: '#131516',
+		hex: '#131516'
 	},
 	{
 		name: 'tomatenrot',
-		hex: '#8A2F28',
+		hex: '#8A2F28'
 	},
 	{
 		name: 'türkisblau',
-		hex: '#47848D',
+		hex: '#47848D'
 	},
 	{
 		name: 'türkisgrün',
-		hex: '#256753',
+		hex: '#256753'
 	},
 	{
 		name: 'ultramarinblau',
-		hex: '#193278',
+		hex: '#193278'
 	},
 	{
 		name: 'umbragrau',
-		hex: '#4C4C47',
+		hex: '#4C4C47'
 	},
 	{
 		name: 'violettblau',
-		hex: '#35496B',
+		hex: '#35496B'
 	},
 	{
 		name: 'wasserblau',
-		hex: '#216D76',
+		hex: '#216D76'
 	},
 	{
 		name: 'weinrot',
-		hex: '#561E27',
+		hex: '#561E27'
 	},
 	{
 		name: 'weißgrün',
-		hex: '#B8CFAD',
+		hex: '#B8CFAD'
 	},
 	{
 		name: 'zinkgelb',
-		hex: '#EBD346',
+		hex: '#EBD346'
 	},
 	{
 		name: 'zitronengelb',
-		hex: '#CFB539',
+		hex: '#CFB539'
 	},
 ];
