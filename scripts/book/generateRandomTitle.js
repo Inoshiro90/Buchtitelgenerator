b2d361944cd58462600dafdf1d04306f0fe63547
Inoshiro90 <110790047+Inoshiro90@users.noreply.github.com>
2024-07-16 15:40:06 +0200

    Improved title generation time

--- scripts/book/generateRandomTitle.js
+++ scripts/book/generateRandomTitle.js
@@ -1,88 +1,152 @@
+// async function generateRandomTitle(i, selectedSettings, selectedGenres) {
+// 	const name = getNames(selectedSettings);
+// 	const noun = getNouns(selectedSettings, nounMapping);
+// 	const adjective = getAdjectives(selectedSettings, adjectiveMapping);
+// 	const location = getLocations(selectedSettings, locationMapping);
+
+// 	const genreFictionArray = await getGenreTitle(noun, adjective, name, location, 'Belletristik')
+// 	const genreBiographyArray = await getGenreTitle(noun, adjective, name, location, 'Biographie')
+// 	const genreLeisureArray = await getGenreTitle(noun, adjective, name, location, 'Freizeit')
+// 	const genreGeographyArray = await getGenreTitle(noun, adjective, name, location, 'Geographie')
+// 	// const genreHistoryArray = getGenreHistoryArray(noun, adjective, name, location);
+// 	const genreHistoryArray = await getGenreTitle(noun, adjective, name, location, 'Geschichte')
+// 	const genreCraftArray = await getGenreTitle(noun, adjective, name, location, 'Handwerk')
+// 	const genreMedicineArray = await getGenreTitle(noun, adjective, name, location, 'Heilkunst')
+// 	const genreChildrensBookArray = await getGenreTitle(noun, adjective, name, location, 'Kinderbuch')
+// 	const genreCulinaryArtsArray = await getGenreTitle(noun, adjective, name, location, 'Kochkunst')
+// 	const genreFineArtsArray = await getGenreTitle(noun, adjective, name, location, 'Bildende Kunst')
+// 	const genrePerformingArtsArray = await getGenreTitle(noun, adjective, name, location, 'Darstellende Kunst')
+// 	const genreMagicArray = await getGenreTitle(noun, adjective, name, location, 'Magie')
+// 	const genreNatureStudiesArray = await getGenreTitle(noun, adjective, name, location, 'Naturkunde')
+// 	const genrePhilosophyArray = await getGenreTitle(noun, adjective, name, location, 'Philosophie')
+// 	const genrePsychologyArray = await getGenreTitle(noun, adjective, name, location, 'Psychologie')
+// 	const genreLawArray = await getGenreTitle(noun, adjective, name, location, 'Recht')
+// 	const genreReligionArray = await getGenreTitle(noun, adjective, name, location, 'Religion')
+// 	const genreSportArray = await getGenreTitle(noun, adjective, name, location, 'Sport')
+// 	const genreLanguageArray = await getGenreTitle(noun, adjective, name, location, 'Sprache')
+// 	const genreEthnologyArray = await getGenreTitle(noun, adjective, name, location, 'Völkerkunde')
+
+// 	// const genreFictionArray = getGenreFictionArray(noun, adjective, name, location);
+// 	// const genreBiographyArray = getGenreBiographyArray(noun, adjective, name, location);
+// 	// const genreLeisureArray = getGenreLeisureArray(noun, adjective, name, location);
+// 	// const genreGeographyArray = getGenreGeographyArray(noun, adjective, name, location);
+// 	// // const genreHistoryArray = getGenreHistoryArray(noun, adjective, name, location);
+// 	// const genreHistoryArray = await getGenreArray('history', noun, adjective, name, location); 
+// 	// const genreCraftArray = getGenreCraftArray(noun, adjective, name, location);
+// 	// const genreMedicineArray = getGenreMedicineArray(noun, adjective, name, location);
+// 	// const genreChildrensBookArray = getGenreChildrensBookArray(noun, adjective, name, location);
+// 	// const genreCulinaryArtsArray = getGenreCulinaryArtsArray(noun, adjective, name, location);
+// 	// const genreFineArtsArray = getGenreFineArtsArray(noun, adjective, name, location);
+// 	// const genrePerformingArtsArray = getGenrePerformingArtsArray(noun, adjective, name, location);
+// 	// const genreMagicArray = getGenreMagicArray(noun, adjective, name, location);
+// 	// const genreNatureStudiesArray = getGenreNatureStudiesArray(noun, adjective, name, location);
+// 	// const genrePhilosophyArray = getGenrePhilosophyArray(noun, adjective, name, location);
+// 	// const genrePsychologyArray = getGenrePsychologyArray(noun, adjective, name, location);
+// 	// const genreLawArray = getGenreLawArray(noun, adjective, name, location);
+// 	// const genreReligionArray = getGenreReligionArray(noun, adjective, name, location);
+// 	// const genreSportArray = getGenreSportArray(noun, adjective, name, location);
+// 	// const genreLanguageArray = getGenreLanguageArray(noun, adjective, name, location);
+// 	// const genreEthnologyArray = getGenreEthnologyArray(noun, adjective, name, location);
+
+// 	const genreArrays = {
+// 		Belletristik: genreFictionArray,
+// 		Biographie: genreBiographyArray,
+// 		Freizeit: genreLeisureArray,
+// 		Geographie: genreGeographyArray,
+// 		Geschichte: genreHistoryArray,
+// 		Handwerk: genreCraftArray,
+// 		Heilkunde: genreMedicineArray,
+// 		Kinderbuch: genreChildrensBookArray,
+// 		Kochkunst: genreCulinaryArtsArray,
+// 		'Bildende Kunst': genreFineArtsArray,
+// 		'Darstellende Kunst': genrePerformingArtsArray,
+// 		Magie: genreMagicArray,
+// 		Naturkunde: genreNatureStudiesArray,
+// 		Philosophie: genrePhilosophyArray,
+// 		Psychologie: genrePsychologyArray,
+// 		Recht: genreLawArray,
+// 		Religion: genreReligionArray,
+// 		Sport: genreSportArray,
+// 		Sprache: genreLanguageArray,
+// 		Völkerkunde: genreEthnologyArray,
+// 	};
+
+// 	const bookTitleIdeas = [];
+
+// 	for (const genre of selectedGenres) {
+// 		const genreArray = genreArrays[genre];
+// 		if (genreArray) {
+// 			const randomTitle = getRandomElement(genreArray);
+// 			if (randomTitle) {
+// 				bookTitleIdeas.push(randomTitle);
+// 			}
+// 		}
+// 	}
+
+// 	const randomTitle = getRandomElement(bookTitleIdeas);
+// 	return randomTitle;
+// }
+
+// async function generateRandomTitle(i, selectedSettings, selectedGenres) {
+//     const name = getNames(selectedSettings);
+//     const noun = getNouns(selectedSettings, nounMapping);
+//     const adjective = getAdjectives(selectedSettings, adjectiveMapping);
+//     const location = getLocations(selectedSettings, locationMapping);
+
+//     const genrePromises = selectedGenres.map(genre => getGenreTitle(noun, adjective, name, location, genre));
+//     const genreArrays = await Promise.all(genrePromises);
+
+//     const genreArraysMapping = {};
+//     selectedGenres.forEach((genre, index) => {
+//         genreArraysMapping[genre] = genreArrays[index];
+//     });
+
+//     const bookTitleIdeas = [];
+
+//     for (const genre of selectedGenres) {
+//         const genreArray = genreArraysMapping[genre];
+//         if (genreArray) {
+//             const randomTitle = getRandomElement(genreArray);
+//             if (randomTitle) {
+//                 bookTitleIdeas.push(randomTitle);
+//             }
+//         }
+//     }
+
+//     const randomTitle = getRandomElement(bookTitleIdeas);
+//     return randomTitle;
+// }
+
 async function generateRandomTitle(i, selectedSettings, selectedGenres) {
-	const name = getNames(selectedSettings);
-	const noun = getNouns(selectedSettings, nounMapping);
-	const adjective = getAdjectives(selectedSettings, adjectiveMapping);
-	const location = getLocations(selectedSettings, locationMapping);
+    const name = getNames(selectedSettings);
+    const noun = getNouns(selectedSettings, nounMapping);
+    const adjective = getAdjectives(selectedSettings, adjectiveMapping);
+    const location = getLocations(selectedSettings, locationMapping);
 
-	const genreFictionArray = await getGenreTitle(noun, adjective, name, location, 'Belletristik')
-	const genreBiographyArray = await getGenreTitle(noun, adjective, name, location, 'Biographie')
-	const genreLeisureArray = await getGenreTitle(noun, adjective, name, location, 'Freizeit')
-	const genreGeographyArray = await getGenreTitle(noun, adjective, name, location, 'Geographie')
-	// const genreHistoryArray = getGenreHistoryArray(noun, adjective, name, location);
-	const genreHistoryArray = await getGenreTitle(noun, adjective, name, location, 'Geschichte')
-	const genreCraftArray = await getGenreTitle(noun, adjective, name, location, 'Handwerk')
-	const genreMedicineArray = await getGenreTitle(noun, adjective, name, location, 'Heilkunst')
-	const genreChildrensBookArray = await getGenreTitle(noun, adjective, name, location, 'Kinderbuch')
-	const genreCulinaryArtsArray = await getGenreTitle(noun, adjective, name, location, 'Kochkunst')
-	const genreFineArtsArray = await getGenreTitle(noun, adjective, name, location, 'Bildende Kunst')
-	const genrePerformingArtsArray = await getGenreTitle(noun, adjective, name, location, 'Darstellende Kunst')
-	const genreMagicArray = await getGenreTitle(noun, adjective, name, location, 'Magie')
-	const genreNatureStudiesArray = await getGenreTitle(noun, adjective, name, location, 'Naturkunde')
-	const genrePhilosophyArray = await getGenreTitle(noun, adjective, name, location, 'Philosophie')
-	const genrePsychologyArray = await getGenreTitle(noun, adjective, name, location, 'Psychologie')
-	const genreLawArray = await getGenreTitle(noun, adjective, name, location, 'Recht')
-	const genreReligionArray = await getGenreTitle(noun, adjective, name, location, 'Religion')
-	const genreSportArray = await getGenreTitle(noun, adjective, name, location, 'Sport')
-	const genreLanguageArray = await getGenreTitle(noun, adjective, name, location, 'Sprache')
-	const genreEthnologyArray = await getGenreTitle(noun, adjective, name, location, 'Völkerkunde')
+    const genrePromises = selectedGenres.map(genre => getGenreTitle(noun, adjective, name, location, genre));
+    const genreArrays = await Promise.all(genrePromises);
 
-	// const genreFictionArray = getGenreFictionArray(noun, adjective, name, location);
-	// const genreBiographyArray = getGenreBiographyArray(noun, adjective, name, location);
-	// const genreLeisureArray = getGenreLeisureArray(noun, adjective, name, location);
-	// const genreGeographyArray = getGenreGeographyArray(noun, adjective, name, location);
-	// // const genreHistoryArray = getGenreHistoryArray(noun, adjective, name, location);
-	// const genreHistoryArray = await getGenreArray('history', noun, adjective, name, location); 
-	// const genreCraftArray = getGenreCraftArray(noun, adjective, name, location);
-	// const genreMedicineArray = getGenreMedicineArray(noun, adjective, name, location);
-	// const genreChildrensBookArray = getGenreChildrensBookArray(noun, adjective, name, location);
-	// const genreCulinaryArtsArray = getGenreCulinaryArtsArray(noun, adjective, name, location);
-	// const genreFineArtsArray = getGenreFineArtsArray(noun, adjective, name, location);
-	// const genrePerformingArtsArray = getGenrePerformingArtsArray(noun, adjective, name, location);
-	// const genreMagicArray = getGenreMagicArray(noun, adjective, name, location);
-	// const genreNatureStudiesArray = getGenreNatureStudiesArray(noun, adjective, name, location);
-	// const genrePhilosophyArray = getGenrePhilosophyArray(noun, adjective, name, location);
-	// const genrePsychologyArray = getGenrePsychologyArray(noun, adjective, name, location);
-	// const genreLawArray = getGenreLawArray(noun, adjective, name, location);
-	// const genreReligionArray = getGenreReligionArray(noun, adjective, name, location);
-	// const genreSportArray = getGenreSportArray(noun, adjective, name, location);
-	// const genreLanguageArray = getGenreLanguageArray(noun, adjective, name, location);
-	// const genreEthnologyArray = getGenreEthnologyArray(noun, adjective, name, location);
+    const genreArraysMapping = {};
+    selectedGenres.forEach((genre, index) => {
+        genreArraysMapping[genre] = genreArrays[index];
+    });
 
-	const genreArrays = {
-		Belletristik: genreFictionArray,
-		Biographie: genreBiographyArray,
-		Freizeit: genreLeisureArray,
-		Geographie: genreGeographyArray,
-		Geschichte: genreHistoryArray,
-		Handwerk: genreCraftArray,
-		Heilkunde: genreMedicineArray,
-		Kinderbuch: genreChildrensBookArray,
-		Kochkunst: genreCulinaryArtsArray,
-		'Bildende Kunst': genreFineArtsArray,
-		'Darstellende Kunst': genrePerformingArtsArray,
-		Magie: genreMagicArray,
-		Naturkunde: genreNatureStudiesArray,
-		Philosophie: genrePhilosophyArray,
-		Psychologie: genrePsychologyArray,
-		Recht: genreLawArray,
-		Religion: genreReligionArray,
-		Sport: genreSportArray,
-		Sprache: genreLanguageArray,
-		Völkerkunde: genreEthnologyArray,
-	};
+    const bookTitleIdeas = [];
 
-	const bookTitleIdeas = [];
+    for (const genre of selectedGenres) {
+        const genreArray = genreArraysMapping[genre];
+        if (genreArray) {
+            const randomTitle = getRandomElement(genreArray);
+            if (randomTitle) {
+                bookTitleIdeas.push(randomTitle);
+            }
+        }
+    }
 
-	for (const genre of selectedGenres) {
-		const genreArray = genreArrays[genre];
-		if (genreArray) {
-			const randomTitle = getRandomElement(genreArray);
-			if (randomTitle) {
-				bookTitleIdeas.push(randomTitle);
-			}
-		}
-	}
+    const randomTitle = getRandomElement(bookTitleIdeas);
+    return randomTitle;
+}
 
-	const randomTitle = getRandomElement(bookTitleIdeas);
-	return randomTitle;
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
--- scripts/book/title/genres/getGenreTitle.js
+++ scripts/book/title/genres/getGenreTitle.js
@@ -1,22 +1,66 @@
+// // async function getGenreTitle(noun, adjective, name, location, genre) {
+// //     // Lade die CSV-Datei
+// //     const response = await fetch('./files/csv/genre/genre.csv');
+// //     const csvText = await response.text();
+
+// //     // Parse die CSV-Datei
+// //     const rows = csvText.split('\n').map(row => row.split(';'));
+
+// //     // Extrahiere die Header und Daten
+// //     const headers = rows[0];
+// //     const data = rows.slice(1);
+
+// //     // Filter die Daten nach dem gewünschten Genre
+// //     const filteredData = data.filter(row => row[headers.indexOf('genre')] === genre);
+
+// //     // Mappe die gefilterten Daten in das gewünschte Format
+// //     const result = filteredData.map(row => {
+// //         const titleTemplate = row[headers.indexOf('title')];
+// //         const tagsTemplate = row[headers.indexOf('tags')];
+
+// //         return {
+// //             title: evalTemplate(titleTemplate, { noun, adjective, name, location }),
+// //             tags: evalTemplate(tagsTemplate, { noun, adjective, name, location }),
+// //         };
+// //     });
+
+// //     return result;
+// // }
+
+// // // Hilfsfunktion zum Evaluieren der Template-Strings
+// // function evalTemplate(template, context) {
+// //     return new Function(...Object.keys(context), `return \`${template}\`;`)(...Object.values(context));
+// // }
+
 // async function getGenreTitle(noun, adjective, name, location, genre) {
 //     // Lade die CSV-Datei
 //     const response = await fetch('./files/csv/genre/genre.csv');
 //     const csvText = await response.text();
 
 //     // Parse die CSV-Datei
-//     const rows = csvText.split('\n').map(row => row.split(';'));
+//     const rows = csvText.split('\n').map(row => row.split(';').map(cell => cell.trim()));
 
 //     // Extrahiere die Header und Daten
 //     const headers = rows[0];
-//     const data = rows.slice(1);
+//     const data = rows.slice(1).filter(row => row.length === headers.length);
+
+//     // Indizes der relevanten Spalten
+//     const titleIndex = headers.indexOf('title');
+//     const tagsIndex = headers.indexOf('tags');
+//     const genreIndex = headers.indexOf('genre');
+
+//     // Überprüfe, ob alle benötigten Spalten vorhanden sind
+//     if (titleIndex === -1 || tagsIndex === -1 || genreIndex === -1) {
+//         throw new Error('CSV file is missing required columns');
+//     }
 
 //     // Filter die Daten nach dem gewünschten Genre
-//     const filteredData = data.filter(row => row[headers.indexOf('genre')] === genre);
+//     const filteredData = data.filter(row => row[genreIndex] === genre);
 
 //     // Mappe die gefilterten Daten in das gewünschte Format
 //     const result = filteredData.map(row => {
-//         const titleTemplate = row[headers.indexOf('title')];
-//         const tagsTemplate = row[headers.indexOf('tags')];
+//         const titleTemplate = row[titleIndex];
+//         const tagsTemplate = row[tagsIndex];
 
 //         return {
 //             title: evalTemplate(titleTemplate, { noun, adjective, name, location }),
@@ -32,46 +76,113 @@
 //     return new Function(...Object.keys(context), `return \`${template}\`;`)(...Object.values(context));
 // }
 
-async function getGenreTitle(noun, adjective, name, location, genre) {
-    // Lade die CSV-Datei
+// let cachedCsvData = null;
+
+// async function loadCsvData() {
+//     if (cachedCsvData) {
+//         return cachedCsvData;
+//     }
+
+//     const response = await fetch('./files/csv/genre/genre.csv');
+//     const csvText = await response.text();
+//     const rows = csvText.split('\n').map(row => row.split(';').map(cell => cell.trim()));
+
+//     const headers = rows[0];
+//     const data = rows.slice(1).filter(row => row.length === headers.length);
+
+//     cachedCsvData = { headers, data };
+//     return cachedCsvData;
+// }
+
+// async function getGenreTitle(noun, adjective, name, location, genre) {
+//     const { headers, data } = await loadCsvData();
+
+//     const titleIndex = headers.indexOf('title');
+//     const tagsIndex = headers.indexOf('tags');
+//     const genreIndex = headers.indexOf('genre');
+
+//     if (titleIndex === -1 || tagsIndex === -1 || genreIndex === -1) {
+//         throw new Error('CSV file is missing required columns');
+//     }
+
+//     const filteredData = data.filter(row => row[genreIndex] === genre);
+
+//     const result = filteredData.map(row => {
+//         const titleTemplate = row[titleIndex];
+//         const tagsTemplate = row[tagsIndex];
+
+//         return {
+//             title: evalTemplate(titleTemplate, { noun, adjective, name, location }),
+//             tags: evalTemplate(tagsTemplate, { noun, adjective, name, location }),
+//         };
+//     });
+
+//     return result;
+// }
+
+// function evalTemplate(template, context) {
+//     return new Function(...Object.keys(context), `return \`${template}\`;`)(...Object.values(context));
+// }
+
+let cachedGenreData = null;
+
+async function loadAndGroupCsvData() {
+    if (cachedGenreData) {
+        return cachedGenreData;
+    }
+
     const response = await fetch('./files/csv/genre/genre.csv');
     const csvText = await response.text();
-
-    // Parse die CSV-Datei
     const rows = csvText.split('\n').map(row => row.split(';').map(cell => cell.trim()));
 
-    // Extrahiere die Header und Daten
     const headers = rows[0];
     const data = rows.slice(1).filter(row => row.length === headers.length);
 
-    // Indizes der relevanten Spalten
     const titleIndex = headers.indexOf('title');
     const tagsIndex = headers.indexOf('tags');
     const genreIndex = headers.indexOf('genre');
 
-    // Überprüfe, ob alle benötigten Spalten vorhanden sind
     if (titleIndex === -1 || tagsIndex === -1 || genreIndex === -1) {
         throw new Error('CSV file is missing required columns');
     }
 
-    // Filter die Daten nach dem gewünschten Genre
-    const filteredData = data.filter(row => row[genreIndex] === genre);
+    const genreDataMap = new Map();
 
-    // Mappe die gefilterten Daten in das gewünschte Format
-    const result = filteredData.map(row => {
+    data.forEach(row => {
+        const genre = row[genreIndex];
         const titleTemplate = row[titleIndex];
         const tagsTemplate = row[tagsIndex];
 
-        return {
-            title: evalTemplate(titleTemplate, { noun, adjective, name, location }),
-            tags: evalTemplate(tagsTemplate, { noun, adjective, name, location }),
+        const entry = {
+            title: titleTemplate,
+            tags: tagsTemplate
         };
+
+        if (!genreDataMap.has(genre)) {
+            genreDataMap.set(genre, []);
+        }
+        genreDataMap.get(genre).push(entry);
     });
 
-    return result;
+    cachedGenreData = genreDataMap;
+    return cachedGenreData;
 }
 
-// Hilfsfunktion zum Evaluieren der Template-Strings
+async function getGenreTitle(noun, adjective, name, location, genre) {
+    const genreDataMap = await loadAndGroupCsvData();
+
+    if (!genreDataMap.has(genre)) {
+        return [];
+    }
+
+    const filteredData = genreDataMap.get(genre);
+
+    return filteredData.map(entry => ({
+        title: evalTemplate(entry.title, { noun, adjective, name, location }),
+        tags: evalTemplate(entry.tags, { noun, adjective, name, location })
+    }));
+}
+
 function evalTemplate(template, context) {
     return new Function(...Object.keys(context), `return \`${template}\`;`)(...Object.values(context));
 }
